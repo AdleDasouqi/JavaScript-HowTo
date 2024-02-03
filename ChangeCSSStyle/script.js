730b179d8434e92961891005936d63e6982bb72b
Adledasouqi <adledasouqi12@outlook.com>
2024-02-03 23:28:13 +0300

    Change CSS Style
    Change CSS Style Example

---
+++ ChangeCSSStyle/script.js
@@ -0,0 +1,9 @@
+function changeStyle() {
+    var styleElement = document.getElementById('changeElement');
+    // styleElement.style.fontSize = "20px";
+    styleElement.innerHTML = 'The style has changed';
+    styleElement.style.fontWeight = "bold";
+    styleElement.style.textAlign = "center";
+    styleElement.style.color = "#fbaf41";
+    styleElement.style.marginRight = "35%";
+}
