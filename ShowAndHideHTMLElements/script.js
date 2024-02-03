20d57278d70c369de4575d5c9b2b74c201ee046a
Adledasouqi <adledasouqi12@outlook.com>
2024-02-03 18:20:01 +0300

    Show and Hide Element
    Show and Hide Element Example

---
+++ ShowAndHideHTMLElements/script.js
@@ -0,0 +1,12 @@
+// This function to show hidden elements.
+function showElement() {
+    var hiddenElement = document.getElementById("hiddenElement");
+    hiddenElement.style.display = "block";
+}
+
+// This function to hidden show elements.
+function hiddenElement() {
+    var showElement = document.getElementById("hiddenElement");
+    showElement.style.display = "none";
+}
+
