ccef1b66ae55accc856d0dd65e89af9232b78457
Adledasouqi <adledasouqi12@outlook.com>
2024-02-04 00:07:19 +0300

    Change HTML Attribute Value
    Change HTML Attribute Value Example

---
+++ ChangeHTMLAttributeValue/script.js
@@ -0,0 +1,8 @@
+// Access the element by its id
+var paragraphElement = document.getElementById("oldId");
+
+// Change the value of the "id" attribute
+paragraphElement.id = "newId"
+
+// Change the content of the paragraph
+paragraphElement.innerHTML = "Attribute 'id' changed, and content updated.";
