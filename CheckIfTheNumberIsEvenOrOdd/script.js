3a74aec341f3b549e2aa2fb2b637132661a6be39
Adledasouqi <adledasouqi12@outlook.com>
2024-02-25 15:44:16 +0300

    Check if the Number is Even  or Odd
    Check if the Number is Even or Odd

--- CheckIfTheNumberIsEvenOrOdd/script.js
+++ CheckIfTheNumberIsEvenOrOdd/script.js
@@ -2,25 +2,18 @@ function inputNumber() {
     // Get the input value from the user
     var number = document.getElementById("inputNumber").value.trim();
 
-    
-    // Check if the input is empty
-    if(number === "") {
-        alert("Please enter a number");
-        return;
-    }
-
-    if(isNaN(number)) {
-        alert("Please enter a valid number");
-        return;
-    }
-
     // Convert the input to a number
     number = parseInt(number);
+    
+    // Check if the input is empty and if the input is contain any letters.
+    if(number === "" || isNaN(number)) {
+        alert("Please enter a valid number");
+        return;
 
-    // Check if the number is even or odd 
-    if(number % 2 === 0) {
+     // Check if the number is even or odd 
+    } else if(number % 2 === 0) {
         document.getElementById("result").innerHTML = "The number is even";
     } else {
-        document.getElementById("result").innerHTML = "The number is odd";        
-    }
+        document.getElementById("result").innerHTML = "The number is odd"; 
+    }  
 }
