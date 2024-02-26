74a8ef45af936d8ceb44b416ccf032aea97a33ca
Adledasouqi <adledasouqi12@outlook.com>
2024-02-26 14:55:06 +0300

    Show and Hide HTML Elements
    Show and Hide HTML Elements Example

--- ShowAndHideHTMLElements/script.js
+++ ShowAndHideHTMLElements/script.js
@@ -1,12 +1,16 @@
 // This function to show hidden elements.
 function showElement() {
-    var hiddenElement = document.getElementById("hiddenElement");
-    hiddenElement.style.display = "block";
+    var showElement = document.getElementById("hiddenElement");
+    showElement.style.display = "block";
+    showElement.style.fontFamily = "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif";
 }
 
 // This function to hidden show elements.
 function hiddenElement() {
-    var showElement = document.getElementById("hiddenElement");
-    showElement.style.display = "none";
+    var hiddenElement = document.getElementById("hiddenElement");
+    hiddenElement.style.display = "none";
 }
+
+document.getElementById("viewElement").style.padding = "12px";
+document.getElementById("hideElement").style.padding = "12px";
 
