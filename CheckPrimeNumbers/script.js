4060cc7057afd554b3edb8d898a455b821149255
Adledasouqi <adledasouqi12@outlook.com>
2024-02-29 16:59:05 +0300

    Check Prime Numbers
    Check Prime Numbers Example

---
+++ CheckPrimeNumbers/script.js
@@ -0,0 +1,35 @@
+function primeNumber() {
+    var inputNumber = document.getElementById("primeNumber").value.trim();
+    var isPrime = true;
+
+    if(isNaN(inputNumber) || inputNumber === "") {
+        alert("Please enter a vaild number");
+        return;
+    }
+
+    inputNumber = Number(inputNumber);
+
+    if(inputNumber === 1) {
+        // A composite number is a natural number or a positive integer that has more than 2 factors.
+        alert("1 is neither prime nor composite number"); 
+        return;
+    }
+
+    if(inputNumber > 1) {
+        for(var i = 2; i < inputNumber; i++) {
+            if(inputNumber % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+    }
+
+    if(isPrime) {
+        document.getElementById("isPrime").innerHTML = `${inputNumber} is a prime number`;
+        console.log(isPrime);
+    } else {
+        document.getElementById("isPrime").innerHTML = `${inputNumber} is not prime number`;
+        console.log(isPrime);
+    }
+    
+}
